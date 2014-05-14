9defceb4d165c51a370c04a43d9b4c0c3e2e217b
Scott Corgan <scottcorgan@gmail.com>
2014-05-14 09:45:05 -0700

    Add more test configuration

--- test/index.js
+++ test/index.js
@@ -2,6 +2,20 @@ var forms = require('../index.js');
 var expect = require('chai').expect;
 var request = require('supertest');
 var connect = require('connect');
+var config = {
+  "forms": {
+    "contact": {
+      "to":"Company Contact <info@your-company.com>",
+      "from":"{{email}}",
+      "subject":"Contact form filled out by {{name}}"
+    },
+    "beta": {
+      "to":"beta@your-company.com",
+      "subject":"Beta Signup",
+      "text":"{{name}} signed up for the private beta."
+    }
+  }
+};
 
 describe('forms service', function () {
   
