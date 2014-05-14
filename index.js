e46e56751c392e3029ca5cfc8bc08f0d6bbb746b
Scott Corgan <scottcorgan@gmail.com>
2014-05-14 11:57:47 -0700

    test: stub out mailing function

--- index.js
+++ index.js
@@ -1,17 +1,74 @@
 var nodemailer = require('nodemailer');
+var _ = require('lodash');
+var mailDefaults = {
+  transport: 'SMTP'
+};
 
-module.exports = function (options) {
+module.exports = function (settings) {
+  var transportName = _.defaults(settings, mailDefaults).transport;
+  var transport = nodemailer.createTransport(transportName, settings.options);
+  
   return function (req, res, next) {
     if (req.method !== 'POST') return next();
     
-    var taskName = parseTaskName(req.service.path);
+    var taskName = req.service.path.replace('/forms/', '').split('/')[0];
+    var taskConfig = req.service.config[taskName];
     
-    if (!taskName || !req.service.config[taskName]) return next();
+    if (!taskName || !taskConfig) return next();
     
+    var mailOptions = _(settings)
+      .omit(['transport', 'options'])
+      .extend(taskConfig)
+      .value();
     
+    // send mail with defined transport object
+    transport.sendMail(mailOptions, function (err, sendResponse){
+      if (isXhr(req)) {
+        res.writeHead(err ? 400 : 200);
+        res.end(err ? err.message :  parseHeaders(sendResponse.message));
+      }
+      else {
+        res.writeHead(302, {
+          'Location': (err) ? taskConfig.success : taskConfig.error
+        });
+        res.end();
+      }      
+    });
   };
 };
 
-function parseTaskName (pathname) {
-  return pathname.replace('/forms/', '').split('/')[0];
+function isXhr (req) {
+  return (req.headers["x-requested-with"] === 'XMLHttpRequest');
+}
+
+function parseHeaders (str) {
+  var headers = _(str.split('\r'))
+    .map(function (data) {
+      return data.replace('\n', '');
+    })
+    .filter(_.identity)
+    .map(function (data) {
+      var obj = {};
+      data = data.split(': ');
+      
+      obj[data[0]] = data[1];
+      
+      return obj;
+    })
+    .reduce(function (obj, data) {
+      var key = Object.keys(data)[0];
+      var val = data[key];
+      
+      // Body of email
+      if (!val) {
+        val = key;
+        key = 'body';
+      }
+      
+      obj[key] = val;
+      
+      return obj;
+    }, {});
+    
+  return JSON.stringify(headers);
 }
--- test/index.js
+++ test/index.js
@@ -7,9 +7,13 @@ var bodyParser = require('body-parser');
 var config = {
   "forms": {
     "contact": {
-      "to":"Company Contact <info@your-company.com>",
-      "from":"{{email}}",
-      "subject":"Contact form filled out by {{name}}"
+      "to":"Scott Corgan <scottcorgan@gmail.com>",
+      // "from":"{{email}}",
+      "from": "Some Guy <scott@divshot.com>",
+      "subject":"Contact form filled out by {{name}}",
+      "text": "HEY THERE!",
+      "success": "/contact?status=success",
+      "error": "/contact?status=error"
     },
     "beta": {
       "to":"beta@your-company.com",
@@ -72,21 +76,41 @@ describe('forms service', function () {
       .end(done);
   });
   
-  it.skip('passes tests', function (done) {
-    app.use(forms());
+  it.only('passes tests', function (done) {
+    app.use(forms({
+      from: 'Some Other Guy <scott@divshot.com>',
+      transport: 'Stub',
+      options: {
+        // service: "Gmail",
+        // auth: {
+        //   user: "scottcorgan@gmail.com",
+        //   pass: "Rad1alp00p!"
+        // }
+      }
+    }));
     
     request(app)
       .post('/forms/contact')
+      .set('x-requested-with', 'XMLHttpRequest')
       .send({
         email: 'test@test.com',
         name: 'test'
       })
-      .end(function (err) {
-        done();
-      });
+      .expect(200)
+      .expect(function (res) {
+        var emailHeaders = JSON.parse(res.text);
+        expect(emailHeaders.From).to.equal('"Some Guy" <scott@divshot.com>');
+      })
+      .end(done);
     
   });
   
   it('parses email templates with values from request');
+  it('to field cannot be dynamic');
+  it('parses form encoded data');
+  it('parses JSON data');
+  it('redirects on non xhr requests');
+  it('returns a 200 on successful send from xhr request with parsed headers');
+  it('returns a 500 on unsuccessful send from xhr request with error message');
   
 });
