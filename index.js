946fae7965c99c35db884ddb2dab3c4507577d73
Scott Corgan <scottcorgan@gmail.com>
2014-05-14 10:28:26 -0700

    tests: skipping middleware

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-module.exports = function () {
+var nodemailer = require('nodemailer');
+
+module.exports = function (options) {
   return function (req, res, next) {
-    next();
+    if (req.method !== 'POST') return next();
+    
+    var taskName = parseTaskName(req.service.path);
+    
+    if (!taskName || !req.service.config[taskName]) return next();
+    
+    
   };
 };
+
+function parseTaskName (pathname) {
+  return pathname.replace('/forms/', '').split('/')[0];
+}
--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@ var forms = require('../index.js');
 var expect = require('chai').expect;
 var request = require('supertest');
 var connect = require('connect');
+var bodyParser = require('body-parser');
+
 var config = {
   "forms": {
     "contact": {
@@ -17,10 +19,74 @@ var config = {
   }
 };
 
+/*
+  Possible options:
+  ====================
+    - to: The email address (and name) of the recipient. This field cannot be dynamic.
+    - reply_to: The reply-to address for the email, for easy followup.
+    - subject: The subject of the email.
+    - html: (optional) an HTML template for the body of the email. Otherwise key value pairs will be displayed nicely.
+    - text: (optional) a plain text template for the body of the email.
+ */
+
 describe('forms service', function () {
+  var app;
   
-  it('passes tests', function () {
-    expect(true).to.equal(true);
+  beforeEach(function () {
+    app = connect()
+      .use(bodyParser())
+      .use(function (req, res, next) {
+        req.service = {
+          name: 'forms',
+          config: config.forms,
+          path: '/forms/contact',
+        };
+        next();
+      });
   });
+  
+  it('skips if it is not a post request', function (done) {
+    app.use(forms());
+    
+    request(app)
+      .get('/')
+      .expect(404)
+      .end(done);
+  });
+  
+  it('skips if the request does not match a config task', function (done) {
+    app
+      .use(function (req, res, next) {
+        req.service = {
+          name: 'forms',
+          config: config.forms,
+          path: req.url,
+        };
+        next();
+      })
+      .use(forms());
+    
+    request(app)
+      .post('/forms/does-not-exist')
+      .expect(404)
+      .end(done);
+  });
+  
+  it.skip('passes tests', function (done) {
+    app.use(forms());
+    
+    request(app)
+      .post('/forms/contact')
+      .send({
+        email: 'test@test.com',
+        name: 'test'
+      })
+      .end(function (err) {
+        done();
+      });
+    
+  });
+  
+  it('parses email templates with values from request');
   
 });
