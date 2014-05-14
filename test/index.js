0e122865a781ddb5abfe61497399248b275be31e
Scott Corgan <scottcorgan@gmail.com>
2014-05-13 17:54:33 -0700

    Set up initial files and test

---
+++ test/index.js
@@ -0,0 +1,12 @@
+var forms = require('../index.js');
+var expect = require('chai').expect;
+var request = require('supertest');
+var connect = require('connect');
+
+describe('forms service', function () {
+  
+  it('passes tests', function () {
+    expect(true).to.equal(true);
+  });
+  
+});
